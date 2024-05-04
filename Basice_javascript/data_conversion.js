e10dbb8db8112b5027708f2d0315099f01d2c4e3
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-05-04 11:41:52 +0000

    data_conversion

---
+++ Basice_javascript/data_conversion.js
@@ -0,0 +1,10 @@
+let score =""
+//true
+// undefined 
+//null 
+//"44abc";
+console.log(typeof (score));
+
+let Num = Number(score);
+console.log(Num);// NaN stands for not a number
+console.log(typeof (Num));
