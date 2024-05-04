3f548a9bd20c28af2570820b8abed61c2607a39f
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-05-04 06:30:46 +0000

    data_types

---
+++ Basice_javascript/Data_types.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+console.log(3+
+    3
+);
+console.log("Rohit"); // ensure that code readibility is important
+
+let age= 22; // number or int datatype
+let name = "Rohit"; // String datatyoe
+let male = true; // boolean datatype
+
+//primitive datatypes
+//number= range - 2^53
+//bigint
+// string
+// bollean- true/false
+// null- which shows the value is empty
+// undefined - shows the value is not defined
+
+// non-primitive datatypes
+// object
+
+console.log(typeof "Rohit")
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
