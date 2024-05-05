cca3ac2364b93c856bfa22bc204740e6478fe204
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-05-05 03:19:26 +0000

    operations

---
+++ Basice_javascript/Operations.js
@@ -0,0 +1,23 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+// concatination
+let str1 = "hello"
+let str2 = " Rohit"
+let str3 = str1 + str2
+console.log(str3)
+
+// preferences between string and number 
+console.log(2+2)
+console.log("2"+"2")
+console.log(2+2+"2")
+console.log("2"+2+2)
+// AS if in console the first variable is  number then whole variables are treated as number and vice versa for number
+
+console.log(2+2-2*2/4)
+console.log(((2+2)-2))
+// try to use parenthesis for complex operations
