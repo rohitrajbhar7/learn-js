f7581a31359e4f2561083bb63d635cfee900e0ab
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-05-30 02:20:32 +0000

    #

--- Basice_javascript/Data_types.js
+++ Basice_javascript/Data_types.js
@@ -8,16 +8,17 @@ let age= 22; // number or int datatype
 let name = "Rohit"; // String datatyoe
 let male = true; // boolean datatype
 
-//primitive datatypes
+//primitive datatypes :- They represent a single value and are immutable, meaning their value cannot be changed.
 //number= range - 2^53
 //bigint
 // string
 // bollean- true/false
 // null- which shows the value is empty
 // undefined - shows the value is not defined
+// symbol -  A unique and immutable data type often used to identify object properties.
 
 // non-primitive datatypes
-// object
+// object, function , date , regular expression , array
 
 console.log(typeof "Rohit")
 console.log(typeof age);
--- Basice_javascript/data_conversion.js
+++ Basice_javascript/data_conversion.js
@@ -4,6 +4,7 @@ let score =""
 //null 
 //"44abc";
 console.log(typeof (score));
+// typeof :- the typeof operator is used to determine the type of a given value.
 
 let Num = Number(score);
 console.log(Num);// NaN stands for not a number
