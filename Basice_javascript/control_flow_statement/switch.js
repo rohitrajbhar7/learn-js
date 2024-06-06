efc923c9b13d89805fa53c62930c14879fd385d0
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-06-06 04:25:51 +0000

    #

---
+++ Basice_javascript/control_flow_statement/switch.js
@@ -0,0 +1,15 @@
+switch ( month="jan") {
+    case "jan":
+        console.log("january month")
+        break;
+    case "feb":
+        console.log("february month")
+        break;
+    case "march":
+            console.log("march month")
+            break;
+
+    default:
+        console.log("none of the above")
+        break;
+}
