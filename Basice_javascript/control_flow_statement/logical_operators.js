5a6722672e8dbabe14d4fd4362f62cf5d083df0e
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-06-06 04:20:00 +0000

    #

---
+++ Basice_javascript/control_flow_statement/logical_operators.js
@@ -0,0 +1,13 @@
+//  && operator
+ let login= true;
+let debit_card= true;
+if(login && debit_card){
+    console.log("you can purchase")
+}
+
+// || operator
+let loginByGmail = true 
+let loginByNumber = false
+if(loginByGmail || loginByNumber){
+    console.log("you are logged in ");
+}
