4ea8b60d2b2df609e849441b10a4a4abc50e06ae
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-06-07 05:56:16 +0000

    #

---
+++ Basice_javascript/loops/break_and_Continue.js
@@ -0,0 +1,15 @@
+for(let i=0; i<=10; i++){
+    console.log(i)
+    if(i==7){
+        console.log(`${i} detected hence thala for a reason`)
+        break;
+    }
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i)
+    if(i==7){
+        console.log(`${i} detected hence thala for a reason`)
+        continue
+    }
+}
