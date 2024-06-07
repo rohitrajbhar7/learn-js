1a3a3be0398df5d73d69acb0dfd73693bf9adcd1
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-06-07 04:35:56 +0000

    #

---
+++ Basice_javascript/control_flow_statement/nullish_coalescing _and_terniary_Operator.js
@@ -0,0 +1,13 @@
+// The "nullish coalescing operator" (??) is a feature in JavaScript that provides a way to handle null and undefined values. It allows you to specify a default value to be used when the left-hand operand is null or undefined. This operator is particularly useful for dealing with optional values and providing fallbacks.
+let name = null;// or undefined
+let defaultName = "Anonymous";
+
+let displayName = name ?? defaultName;  // nullish coalescing expression
+console.log(displayName); // Output: "Anonymous"
+
+
+
+// The ternary operator, also known as the conditional operator, is a concise way to perform conditional logic in JavaScript. It is the only operator in JavaScript that takes three operands. The ternary operator is often used as a shorthand for the if-else statement.
+let age = 18;
+let isAdult = (age >= 18) ? "Yes" : "No";
+console.log(isAdult); // Output: "Yes"
