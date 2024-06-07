db38762c680118bb4aaba0bdf38748de61c36d36
rohitrajbhar7 <96934654+rohitrajbhar7@users.noreply.github.com>
2024-06-07 06:20:32 +0000

    #

---
+++ Basice_javascript/loops/while_and_DoWhile_loop.js
@@ -0,0 +1,21 @@
+// let i= 0
+// while(i<10){
+//     console.log(`${i}`)
+//      i=i+2;
+// }
+
+// while loop for string array 
+let marvel = ["ironman", "thor", "black widow"]
+let i=0;
+while( i <marvel.length){
+    console.log(` marvel heroes are ${marvel[i]}` )
+        i=i+1;
+   
+}
+
+// do while 
+let score=11 //0
+do{
+    console.log(score);
+    score++
+}while(score <=10)
